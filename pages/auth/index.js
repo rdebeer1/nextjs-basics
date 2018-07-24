9e130392a6a33e044bffc331084e27a9afa87514
Robert DeBeer <Robert.DeBeer@gmail.com>
2018-07-24 14:34:10 -0500

    implements a special lifecycle hook getInitialProps

--- pages/auth/index.js
+++ pages/auth/index.js
@@ -2,11 +2,22 @@ import React from 'react';
 
 import User from '../../components/User';
 
-const authIndexPage = () => (
+const authIndexPage = (props) => (
   <div>
-    <h1>The Auth Index Page</h1>
+    <h1>The Auth Index Page - {props.appName}</h1>
     <User name='Robert' age={26} />
   </div>
 );
+
+authIndexPage.getInitialProps = (context) => {
+  const promise = new Promise((resolve, resject) => {
+    setTimeout(() => {
+      resolve({
+        appName: 'nextjs-basics (Auth)'
+      });
+    }, 1000);
+  });
+  return promise;
+}
 
 export default authIndexPage;
